5ec2d0f1284719f57d443f22bd55be9753a9b189
viocha <1457174277@qq.com>
2025-06-06 12:12:00 +0800

    update

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,56 +1,59 @@
 // /api/proxy.ts
 
 export const config = {
-    runtime: 'edge', // 指定这是一个 Edge Function
+	runtime: 'edge', // 指定这是一个 Edge Function
 };
 
 export default async function handler(req: Request): Promise<Response> {
-    // 设置 CORS 头部
-    const corsHeaders = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Headers': '*',
-    };
+	// 设置 CORS 头部
+	const origin = new URL(req.url).origin;
+	const headers = {
+		'Access-Control-Allow-Origin': origin,
+		'Access-Control-Allow-Methods': '*',
+		'Access-Control-Allow-Headers': '*',
+		'Access-Control-Allow-Credentials': 'true',
+		'Accept': 'application/json',
+	};
 
-    // 处理 OPTIONS 请求
-    if (req.method === 'OPTIONS') {
-        return new Response(null, {status: 204, headers: corsHeaders});
-    }
+	// 处理 OPTIONS 请求
+	if (req.method === 'OPTIONS') {
+		return new Response(null, {status: 204, headers: headers});
+	}
 
-    // 处理非 POST 请求
-    if (req.method !== 'POST') {
-        return Response.json({error: 'Method not allowed',}, {
-            status: 405,
-            headers: corsHeaders,
-        })
-    }
+	// 处理非 POST 请求
+	if (req.method !== 'POST') {
+		return Response.json({error: 'Method not allowed'}, {
+			status: 405,
+			headers: headers,
+		});
+	}
 
-    try {
-        // 获取请求体
-        let {url, method = 'GET', headers = {}, data = null} = await req.json()
+	try {
+		// 获取请求体
+		let {url, method = 'GET', headers = {}, body = null} = await req.json();
 
-        if (!url) {
-            return Response.json({error: 'Missing url in request body'}, {
-                status: 400,
-                headers: corsHeaders,
-            })
-        }
+		if (!url) {
+			return Response.json({error: 'Missing url in request body'}, {
+				status: 400,
+				headers: headers,
+			});
+		}
 
-        // fetch不能带有请求体的方法
-        if (['GET', 'HEAD'].includes(method)) {
-            data = null;
-        }
+		// fetch不能带有请求体的方法
+		if (['GET', 'HEAD'].includes(method)) {
+			body = null;
+		}
 
-        // 使用 fetch 进行请求
-        return await fetch(url, {
-            method,
-            headers,
-            body: data,
-        })
-    } catch (error: any) { // 错误处理
-        return Response.json({error: error.message}, {
-            status: 500,
-            headers: corsHeaders,
-        });
-    }
+		// 使用 fetch 进行请求
+		return await fetch(url, {
+			method,
+			headers,
+			body,
+		});
+	} catch (error: any) { // 错误处理
+		return Response.json({error: error.message}, {
+			status: 500,
+			headers: headers,
+		});
+	}
 }
